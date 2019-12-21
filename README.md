88d530d6b429a498a03e716e35a14bf5d99c99f7
AdamRight <1131045891@qq.com>
2019-12-21 16:53:20 +0800

    引入id

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Kotlin项目
 
 动态切换模块是library还是application。设置gradle.properties中isUserModule。
 
+## 2、引入id
+
+使用 `apply plugin: 'kotlin-android-extensions'`。
 
 
 
--- UserCenter/src/main/java/com/ktea/user/ui/activity/RegisterActivity.kt
+++ UserCenter/src/main/java/com/ktea/user/ui/activity/RegisterActivity.kt
@@ -3,11 +3,16 @@ package com.ktea.user.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.ktea.user.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnRegister.setOnClickListener {
+            btnRegister.text = "注册成功啦"
+        }
     }
 }
