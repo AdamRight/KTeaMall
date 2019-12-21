4100bdb3e3ee37d73926c6805a4854bfa0a0579f
AdamRight <1131045891@qq.com>
2019-12-21 14:30:45 +0800

    初始化

---
+++ app/src/main/java/com/ktea/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ktea.mall.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ktea.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
